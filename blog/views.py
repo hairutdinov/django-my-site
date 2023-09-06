6ad0e2f7cbde3c5df36ddbcb3fd54016e7edc45e
bulat <bulatxajrutdinoff@yandex.ru>
2023-09-06 08:36:48 +0300

    adding urls and empty views

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponseNotFound, HttpResponseRedirect, Http404
+from django.urls import reverse
 
 # Create your views here.
+
+
+def starting_page(request):
+    pass
+
+
+def posts(request):
+    pass
+
+
+def post_detail(request):
+    pass
