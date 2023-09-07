8677b1bc91867820ed3423e9773d854b32fb0856
bulat <bulatxajrutdinoff@yandex.ru>
2023-09-07 20:17:13 +0300

    creating Post, Author, Tag models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
 # Create your models here.
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField()
+
+
+class Tag(models.Model):
+    caption = models.CharField(max_length=20)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=150)
+    excerpt = models.CharField(max_length=200)
+    image_name = models.CharField(max_length=100)
+    date = models.DateField(auto_now=True)
+    slug = models.SlugField(unique=True)
+    content = models.TextField(validators=[MinValueValidator(10)])
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL)
+    tags = models.ManyToManyField(Tag, related_name='tags')
