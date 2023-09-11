d2e1da66c65447d14a846a2599fd0960e4288ed2
bulat <bulatxajrutdinoff@yandex.ru>
2023-09-11 09:35:25 +0300

    refactoring starting page: covert to class-based view

--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,22 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseNotFound, HttpResponseRedirect, Http404
 from django.urls import reverse
-
+from django.views import View
+from django.views.generic.base import TemplateView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
 from .models import Post
 
 
-def starting_page(request):
-    latest_posts = Post.objects.all().order_by('-date')[:3]
-    return render(request, 'blog/index.html', {
-        'posts': latest_posts,
-    })
+class StartingPageView(ListView):
+    template_name = 'blog/index.html'
+    model = Post
+    context_object_name = 'posts'  # by default: object_list
+
+    def get_queryset(self):
+        base_query = super().get_queryset()
+        data = base_query.order_by('-date')[:3]
+        return data
 
 
 def posts(request):
